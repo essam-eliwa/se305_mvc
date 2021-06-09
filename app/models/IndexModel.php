2ac5c1bd25c4279fa0eda5960ed4cd51ebce66c8
Essam Eliwa <essam.eliwa@gmail.com>
2021-06-09 20:09:43 +0200

    minor

--- app/models/IndexModel.php
+++ app/models/IndexModel.php
@@ -1,5 +1,5 @@
 <?php
 class IndexModel extends model{
-     public $title = 'MIU SE305 Blog';
+     public $title = 'MIU SE305 Blog V1.0';
      public $subtitle = 'Example of MVC PHP framework';
 }
